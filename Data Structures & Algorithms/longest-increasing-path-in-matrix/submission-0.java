225be58725e89cf712ebfa7fee4355aadbff19da
shreyasNaik0101 <shreyas.naik0805@gmail.com>
2026-07-23 17:59:38 +0530

    Add: longest-increasing-path-in-matrix - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    int[][] dp;
+
+    public int dfs(int[][] matrix, int r, int c, int preVal){
+        int ROWS = matrix.length;
+        int COLS = matrix[0].length;
+        if(r < 0 || r >= ROWS || c < 0 || c >= COLS || matrix[r][c] <= preVal){
+            return 0;
+        }
+        if(dp[r][c] != -1) return dp[r][c];
+
+        int res = 1;
+        for(int[] d : directions){
+            res = Math.max(res, 1 + dfs(matrix, r+d[0], c+d[1], matrix[r][c]));
+        }
+        return dp[r][c] = res;
+    }
+    public int longestIncreasingPath(int[][] matrix) {
+        int ROWS = matrix.length;
+        int COLS = matrix[0].length;
+        int LIP = 0;
+        dp = new int[ROWS][COLS];
+        for(int i = 0; i < ROWS; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        for(int i=0; i<ROWS; i++){
+            for(int j=0; j<COLS; j++){
+                LIP = Math.max(LIP, dfs(matrix, i, j, Integer.MIN_VALUE));
+            }
+        }
+        return LIP;
+    }
+}
